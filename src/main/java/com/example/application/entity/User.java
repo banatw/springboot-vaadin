8134fcf84090bc70b9cb8f7adce21cf145d8e027
syabana.wahyudi <syabana.wahyudi@gmail.com>
2020-10-31 21:09:26 +0700

    balik lagi ke h2

--- src/main/java/com/example/application/entity/User.java
+++ src/main/java/com/example/application/entity/User.java
@@ -1,19 +1,17 @@
 package com.example.application.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 @Data
 @RequiredArgsConstructor
 @Entity
 @NoArgsConstructor
-@Table(name = "user_")
 public class User {
     @Id
     @NonNull
